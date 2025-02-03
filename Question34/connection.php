8579bc4fa6727c02632347164ebb12671515b943
Jibril9 <154311166+Jibril9@users.noreply.github.com>
2025-02-03 09:49:24 +0100

    Add files via upload

---
+++ Question34/connection.php
@@ -0,0 +1,15 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="jibril";
+$dbc=mysqli_connect($host,$user,$password,$db);
+if($dbc){
+   // echo "connected";
+
+}else{
+   // echo "Not conneted";
+}
+
+
+?>
